08792afc500728fc7df6080348268378ad11cbb1
Anushree <anushreejaiswal.srn@gmail.com>
2023-05-15 00:44:47 +0530

    added navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,55 @@
+import { Stack } from "@mui/material"
+import Image from "next/image"
+import Link from "next/link"
+
 export const Navbar = () => {
     return (
-        <nav>
-            Navbar
-        </nav>
+        <Stack
+            direction={'row'}
+            justifyContent={'none'}
+            sx={{
+                gap: { sm: '122px', xs: '40px' },
+                mt: { sm: '32px', xs: '20px' },
+            }}
+            px={'20px'}
+        >
+            <Link href={'/'}>
+                <Image 
+                    src={'/images/Logo.png'} 
+                    width={48} 
+                    height={48} 
+                    alt="Logo" 
+                    style={{
+                        margin: '0px 20px', 
+                    }} 
+                />
+            </Link>
+
+            <Stack
+                direction={'row'}
+                gap={'40px'}
+                fontSize={"24px"}
+                alignItems={'flex-end'}
+            >
+                <Link 
+                    href={'/'}
+                    style={{
+                        textDecoration: 'none',
+                        color: '#3a1212',
+                        borderBottom: '3px solid #ff2625'
+                    }}
+                >
+                    Home
+                </Link>
+
+                <a
+                    href="#exercises"
+                    style={{
+                        textDecoration: 'none',
+                        color:  '#3a1212'
+                    }}
+                >Exercise</a>
+            </Stack>
+        </Stack>
     )
 }
