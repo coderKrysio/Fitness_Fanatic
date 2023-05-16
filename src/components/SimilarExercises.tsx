7ee695c7576ef35bbf50d3747925e9d275a5d06a
Anushree <anushreejaiswal.srn@gmail.com>
2023-05-16 16:39:52 +0530

    added components

--- src/components/SimilarExercises.tsx
+++ src/components/SimilarExercises.tsx
@@ -1,5 +1,30 @@
-export const SimilarExercises = () => {
+import { Box, Stack, Typography } from "@mui/material"
+import { HorizontalScrollbar } from "./HorizontalScrollbar"
+
+export const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }: any) => {
     return (
-        <div></div>
+        <Box
+            sx={{
+                mt: { lg: '100px', xs: '0'}
+            }}
+        >
+            <Typography
+                variant="h3"
+            >
+                Exercises that target the same muscle group
+            </Typography>
+
+            <Stack
+                direction={"row"}
+                sx={{
+                    p: '2',
+                    position: 'relative',
+                }}
+            >
+                {targetMuscleExercises.length && 
+                    <HorizontalScrollbar data={targetMuscleExercises} />
+                }
+            </Stack>
+        </Box>
     )
 }
