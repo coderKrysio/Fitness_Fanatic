9beda583367494c19008f2ae8b1ad2e3d264f190
Anushree <anushreejaiswal.srn@gmail.com>
2023-05-16 16:33:25 +0530

    added props

--- src/components/ExerciseDetail.tsx
+++ src/components/ExerciseDetail.tsx
@@ -8,6 +8,8 @@ import { exerciseOptions, fetchData, youtubeOptions } from "@/utils/fetchData"
 export const ExerciseDetail = ({ id }: any) => {
     const [exerciseDetail, setExerciseDetail] = useState({});
     const [exerciseVideos, setExerciseVideos] = useState([]);
+    const [targetMuscleExercises, setTargetMuscleExercises] = useState([]);
+    const [equipmentExercises, setEquipmentExercises] = useState([]);
 
     useEffect(() => {
         const fetchExerciseData = async () => {
@@ -19,6 +21,12 @@ export const ExerciseDetail = ({ id }: any) => {
 
             const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?q=${exerciseDetailData.name}`, youtubeOptions);
             setExerciseVideos(exerciseVideosData);
+
+            const targetMuscleExercisesData = await fetchData(`${exerciseDbUrl}/exercises/target/${id}`, exerciseOptions);
+            setTargetMuscleExercises(targetMuscleExercisesData);
+
+            const equipmentExercisesData = await fetchData(`${exerciseDbUrl}/exercises/equipment/${id}`, exerciseOptions);
+            setEquipmentExercises(equipmentExercisesData);
         }
 
         fetchExerciseData();
@@ -29,11 +37,16 @@ export const ExerciseDetail = ({ id }: any) => {
             <Detail 
                 exerciseDetail={exerciseDetail} 
             />
+
             <ExerciseVideos 
                 exerciseVideos={exerciseVideos} 
                 name={(exerciseDetail as any).name} 
             />
-            <SimilarExercises />
+
+            <SimilarExercises
+                targetMuscleExercises={targetMuscleExercises}
+                equipmentExercises={equipmentExercises}
+            />
         </Box>        
     )
 }
