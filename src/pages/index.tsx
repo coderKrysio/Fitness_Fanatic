b460d38ac9dbf1df23c13ba4e4d5eb23742a5c97
Anushree <anushreejaiswal.srn@gmail.com>
2023-05-17 23:09:43 +0530

    added head tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,24 @@
 import { Footer } from '@/components/Footer';
+import { Head } from '@/components/Head';
 import { HomePage } from '@/components/HomePage';
 import { Navbar } from '@/components/Navbar';
 import { Box } from '@mui/material';
 
 export default function Home() {
   return (
-    <Box
-      width={"400px"}
-      sx={{
-        width: { xl: '1488px', }
-      }}
-      m={"auto"}
-    >
-      <Navbar />
-      <HomePage />
-      <Footer />
-    </Box>
+    <>
+      <Head />
+      <Box
+        width={"400px"}
+        sx={{
+          width: { xl: '1488px', }
+        }}
+        m={"auto"}
+      >
+        <Navbar />
+        <HomePage />
+        <Footer />
+      </Box>
+    </>
   )
 }
